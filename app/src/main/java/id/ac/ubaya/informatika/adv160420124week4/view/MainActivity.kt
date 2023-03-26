eb41a63327b9824f8e8e75e486e069da94eaf6b2
marcelladiva <marcelladiva23@gmail.com>
2023-03-26 21:09:50 +0700

    Homework Week4

---
+++ app/src/main/java/id/ac/ubaya/informatika/adv160420124week4/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.ubaya.informatika.adv160420124week4.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ubaya.informatika.adv160420124week4.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/id/ac/ubaya/informatika/adv160420124week4/view/StudentDetailFragment.kt
@@ -0,0 +1,42 @@
+package id.ac.ubaya.informatika.adv160420124week4.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputEditText
+import id.ac.ubaya.informatika.adv160420124week4.R
+import id.ac.ubaya.informatika.adv160420124week4.viewmodel.DetailViewModel
+
+class StudentDetailFragment : Fragment() {
+    private lateinit var detailViewModel: DetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        detailViewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        detailViewModel.fetch()
+
+        val txtStudentID = view.findViewById<TextInputEditText>(R.id.txtID)
+        val txtStudentName = view.findViewById<TextInputEditText>(R.id.txtName)
+        val txtBod = view.findViewById<TextInputEditText>(R.id.txtBod)
+        val txtPhone = view.findViewById<TextInputEditText>(R.id.txtPhone)
+
+        detailViewModel.studentLD.observe(viewLifecycleOwner) { studentDetail ->
+            txtStudentID.setText(studentDetail.id.toString())
+            txtStudentName.setText(studentDetail.name.toString())
+            txtBod.setText(studentDetail.dob.toString())
+            txtPhone.setText(studentDetail.phone.toString())
+        }
+    }
+}
